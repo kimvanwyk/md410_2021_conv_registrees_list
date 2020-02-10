6f79d05926095a3d11e630e509ba837c5bb11d8b
Kim van Wyk <kimv@sahomeloans.com>
2020-02-10 17:25:10 +0200

    Setting margins and borders

--- md410_2020_conv_registree_list/build_registrees_list.py
+++ md410_2020_conv_registree_list/build_registrees_list.py
@@ -7,18 +7,23 @@ __version__ = "0.0.1"
 
 from collections import namedtuple
 from openpyxl import Workbook
-from openpyxl.utils import get_column_letter
+from openpyxl.styles.borders import Border, Side
+from openpyxl.styles import Alignment
+from openpyxl.styles import Font
 
+THIN_BORDER = Border(left=Side(style="thin"), right=Side(style="thin"), top=Side(style="thin"), bottom=Side(style="thin"))
 COLUMN = namedtuple("COLUMN", ("title", "width", "column_name"))
 
-TITLE_HEIGHT = 1.5
-NORMAL_HEIGHT = 1
-ROWS_PER_PAGE = 22
+TITLE_HEIGHT = 40
+NORMAL_HEIGHT = 25
+ROWS_PER_PAGE = 31
+BOLD = Font(name="Arial", bold=True)
+CENTRE = Alignment(horizontal="center", vertical="center")
 COLUMNS = [
-    COLUMN("Name", 7.5, "A"),
-    COLUMN("Reg\nNumber", 1.5, "B"),
-    COLUMN("Status", 4, "C"),
-    COLUMN("Signature – registered\nand gift bag received\n(if applicable)", 4, "D"),
+    COLUMN("Name", 35, "A"),
+    COLUMN("Reg\nNumber", 10, "B"),
+    COLUMN("Status", 15, "C"),
+    COLUMN("Signature – registered\nand gift bag received\n(if applicable)", 35, "D"),
 ]
 
 wb = Workbook()
@@ -26,6 +31,11 @@ wb = Workbook()
 dest_filename = "registrees_list.xlsx"
 ws1 = wb.active
 ws1.title = "By Name"
+ws1.page_setup.paperSize = ws1.PAPERSIZE_A4
+ws1.page_margins.top = 0.2
+ws1.page_margins.bottom = 0.2
+ws1.page_margins.left = 0.2
+ws1.page_margins.right = 0.2
 
 for column in COLUMNS:
     ws1.column_dimensions[column.column_name].width = column.width
@@ -34,10 +44,14 @@ while row < 40:
     if not ((row - 1) % ROWS_PER_PAGE):
         for column in COLUMNS:
             ws1[f"{column.column_name}{row}"] = column.title
-        ws1.row_dimensions[row] = TITLE_HEIGHT
+            ws1[f"{column.column_name}{row}"].font = BOLD
+            ws1[f"{column.column_name}{row}"].alignment = CENTRE
+            ws1[f"{column.column_name}{row}"].border = THIN_BORDER
+        ws1.row_dimensions[row].height = TITLE_HEIGHT
     else:
-        ws1[f"A{row}"] = row
-        ws1.row_dimensions[row] = NORMAL_HEIGHT
+        for column in COLUMNS:
+            ws1[f"{column.column_name}{row}"].border = THIN_BORDER
+        ws1.row_dimensions[row].height = NORMAL_HEIGHT
     row += 1
 
 wb.save(filename=dest_filename)
